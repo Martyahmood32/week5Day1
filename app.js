62e7c6138193b1d6f30a38d23861c2d517022052
DJ <DJ@Dariuss-MacBook-Pro.local>
2022-10-25 14:58:11 -0400

    homework

---
+++ app.js
@@ -0,0 +1,28 @@
+//var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//function add (a, b){
+//return a + b
+//}
+
+//console.log(add(2,5))
+//console.log(add(2, 5, 3)
+
+
+//var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//arr.push(11); 
+
+
+
+//let array = [1, 2, 3, 4, 5];
+
+//let newArray = array.splice(3, 1);
+
+//console.log(add(3, 1))
+//console.log(add(3, 1, 4))
+
+
+//var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//var sliceArray = Array.slice(2, 6)
+
+//console.log(sliceArray)
